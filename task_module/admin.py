74e29cc2ec27fcbfbdb15861ed9b886e13da2569
Caliber24 <amirhosseinpishroa2001@gmail.com>
2024-12-05 13:00:04 +0330

    Update admin.py

--- task_module/admin.py
+++ task_module/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Task
 # Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+  list_display = ['name', 'task_group', 'start_date', 'end_date', 'is_complete']
+  list_editable =['task_group','start_date', 'end_date', 'is_complete']
+  list_filter=['task_group', 'is_complete']
+  ordering = ['name', 'start_date', 'end_date']
